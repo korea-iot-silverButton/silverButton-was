158fe06e59f0e63e028b005ae175f9a610edae47
goshek <gsh6920@naver.com>
2024-11-06 16:16:21 +0900

    afafefa

--- silver-button/src/main/java/com/korit/silverbutton/repository/ScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.korit.silverbutton.repository;
-
-import com.korit.silverbutton.entity.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleRepository extends JpaRepository<Schedule,Long> {
-}
