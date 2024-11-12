5b5bd097a12f9bd9391af7fd91b207bee92a5075
goshek <gsh6920@naver.com>
2024-11-12 12:30:31 +0900

    aaaaaaaaaa

---
+++ silver-button/src/main/java/com/korit/silverbutton/common/constant/ApiMappingPattern.java
@@ -0,0 +1,12 @@
+package com.korit.silverbutton.common.constant;
+
+public class ApiMappingPattern {
+    public static final String AUTH = "/api/v1/auth";
+
+//    public static final String USER = "/api/v1/users";
+//    public static final String POST = "/api/v1/posts";
+//    public static final String BOOK = "/api/v1/books";
+//
+//    public static final String MENU = "/api/v1/menus";
+//    public static final String GROUP = "/api/v1/group";
+}
---
+++ silver-button/src/main/java/com/korit/silverbutton/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.korit.silverbutton.repository;
+
+import com.korit.silverbutton.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserId(String userId);
+
+    boolean existsByUserId(String userId);
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+}
