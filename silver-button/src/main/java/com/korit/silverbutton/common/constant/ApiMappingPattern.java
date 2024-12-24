a7f3625634fa54d00edd1fcb1853b891a9d3dec8
earthmin <fltk4286@naver.com>
2024-12-24 12:06:39 +0900

    20241224 jsm

--- silver-button/src/main/java/com/korit/silverbutton/common/constant/ApiMappingPattern.java
+++ silver-button/src/main/java/com/korit/silverbutton/common/constant/ApiMappingPattern.java
@@ -1,6 +1,8 @@
 package com.korit.silverbutton.common.constant;
 
 public class ApiMappingPattern {
+
     public static final String AUTH = "/api/v1/auth";
+    public static final String DEPENDENT = "/api/v1/dependent";
     public static final String SCEHDULE= "/api/v1/schedule";
 }
---
+++ silver-button/src/main/java/com/korit/silverbutton/dto/User/Request/UserRequestDto.java
@@ -0,0 +1,17 @@
+package com.korit.silverbutton.dto.User.Request;
+
+import lombok.*;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestDto {
+
+    private Long id;
+    private String userId;
+    private String phone;
+    private String email;
+    private String nickname;
+    private String password;
+}
