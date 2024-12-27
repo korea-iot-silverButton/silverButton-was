b068887265ddf922381e9ce5f6b71b36f27df926
jaewon0615 <ujung8926@naver.com>
2024-12-27 17:25:14 +0900

    20241227

--- silver-button/src/main/java/com/korit/silverbutton/common/constant/ApiMappingPattern.java
+++ silver-button/src/main/java/com/korit/silverbutton/common/constant/ApiMappingPattern.java
@@ -6,4 +6,5 @@ public class ApiMappingPattern {
     public static final String DEPENDENT = "/api/v1/dependent";
     public static final String SCEHDULE= "/api/v1/schedule";
     public static final String BOARD = "/api/v1/board";
+    public static final String MEDICINE= "api/v1/medicine";
 }
