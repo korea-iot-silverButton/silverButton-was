c1235bde1ff788cbbb18eaa8e679670ceffa5f8a
earthmin <fltk4286@naver.com>
2025-01-06 11:36:17 +0900

    20250106 jsm update

---
+++ silver-button/src/main/java/com/korit/silverbutton/dto/Mail/SendMailRequestDto.java
@@ -0,0 +1,14 @@
+package com.korit.silverbutton.dto.Mail;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SendMailRequestDto {
+
+    private String email;
+    private String username;
+}
