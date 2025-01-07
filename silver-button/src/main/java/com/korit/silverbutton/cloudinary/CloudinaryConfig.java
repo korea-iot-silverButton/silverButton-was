b8e902201d5187bc15b103208edfcc6f31112b96
jaewon0615 <ujung8926@naver.com>
2025-01-07 10:36:11 +0900

    20250107jjw

--- silver-button/src/main/java/com/korit/silverbutton/cloudinary/CloudinaryConfig.java
+++ silver-button/src/main/java/com/korit/silverbutton/cloudinary/CloudinaryConfig.java
@@ -2,6 +2,7 @@ package com.korit.silverbutton.cloudinary;
 import com.cloudinary.Cloudinary;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
