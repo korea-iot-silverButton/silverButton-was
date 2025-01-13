3a5e7b8b88f5b1a1717d9f396570a87947f336c2
sanggi9900 <tkdrl9900@gmail.com>
2025-01-13 17:31:10 +0900

    20250113

--- silver-button/src/main/java/com/korit/silverbutton/dto/boardlike/Response/BoardLikeResponseDto.java
+++ silver-button/src/main/java/com/korit/silverbutton/dto/boardlike/Response/BoardLikeResponseDto.java
@@ -1,18 +1,27 @@
 package com.korit.silverbutton.dto.boardlike.Response;
 
-import lombok.AllArgsConstructor;
+
+import com.korit.silverbutton.dto.board.Response.BoardResponseDto;
+import com.korit.silverbutton.entity.BoardLike;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
-public class BoardLikeResponseDto {
+public class BoardLikeResponseDto extends BoardResponseDto {
     private Long id;
     private Long boardId;
     private Long likerId;
     private int likes;
 
+    public BoardLikeResponseDto(BoardLike boardLike) {
+        this.id = boardLike.getId();
+        this.boardId = boardLike.getBoard().getId();
+        this.likerId = boardLike.getLiker().getId();
+        this.likes = boardLike.getBoard().getLikes();
+    }
+
+    // 새로운 생성자 추가
     public BoardLikeResponseDto(Long boardId, int likes) {
         this.boardId = boardId;
         this.likes = likes;
