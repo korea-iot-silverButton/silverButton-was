e11fcd5937e9699ef29d1604493998487b439397
guswl458@gmail.com <guswl458@gmail.com>
2025-01-14 11:19:07 +0900

    20250114

--- silver-button/src/main/java/com/korit/silverbutton/repository/MessageRepository.java
+++ silver-button/src/main/java/com/korit/silverbutton/repository/MessageRepository.java
@@ -4,6 +4,7 @@ import com.korit.silverbutton.entity.Message;
 import com.korit.silverbutton.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,11 +19,9 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
 //ORDER BY m.createdAt DESC
 //""")
     @Query("SELECT m FROM Message m WHERE m.sender.id = :userId OR m.receiver.id = :userId")
-    List<Message> findMessageById(Long id);
+    List<Message> findMessageById(@Param("userId") Long id);
 
     List<Message> findAllBySender(User sender); // 발신자 ID로 메시지 조회
-    List<Message> findAllByReceiverId(Long receiver);
-//    List<Message> findById(User user);
+    List<Message> findAllByReceiver(User receiver);
 
-//    List<Object[]> findAllMessages(); - 쿼리쓸때
 }
