62670c8bfdc6f9245fdf41f51a22a749cff5790e
sanggi9900 <tkdrl9900@gmail.com>
2025-01-14 11:21:32 +0900

    20250114

--- silver-button/src/main/java/com/korit/silverbutton/controller/BoardLikeController.java
+++ silver-button/src/main/java/com/korit/silverbutton/controller/BoardLikeController.java
@@ -8,7 +8,6 @@ import com.korit.silverbutton.principal.PrincipalUser;
 import com.korit.silverbutton.service.BoardLikeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
